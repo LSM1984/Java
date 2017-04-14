0a59224bc273f28822e4fde1b7cecf46241a6659
lsm1984github.io <loganmullen123@gmail.com>
2017-04-14 09:55:47 -0700

    Add files via upload

--- code2/TrigTableDeg.java
+++ code2/TrigTableDeg.java
@@ -1,20 +1,21 @@
 // name file TrigTableRad.java
 // compile javac TrigTableRad.java
 // run		java TrigTableRad.java
-class TrigTableRad {
+class TrigTableDeg{
 
 	public static void main (String args[]){
 	    double c,s,t; //temp double for cosine, sine and tangent so they can be rounded
 	    int d = 0; // degress
 	    double r =0, rround = 0;// radian measure
 	    double pi = 3.14159265358979323846;
-	   double i = (pi/12); //increment r 15 degrees equals pi radians
+	   int i = 15; //increment r 15 degrees equals pi radians
 	    String un = "UN";
 	    System.out.println("radian measurement is represented as rad");
-	    System.out.println("\trad \t|\tcos\t|\tsin\t|\ttan" );
+	    System.out.println("\tdeg \t|\tcos\t|\tsin\t|\ttan" );
 	    System.out.println("----------------------------------------------------------------------");
-	    while (r <= (2*pi)){
+	    while (d <= 360){
 	        //convert angle t to radians r
+	        r = d * (pi/180.0);
 	        c = Math.cos(r);
 	        c = (Math.round(c *1000.0))/1000.0;
 	        s = Math.sin(r);
@@ -24,11 +25,11 @@ class TrigTableRad {
 	        rround = (Math.round(r*1000.0))/1000.0;
 	        
 	        if (c == 0.0)
-	            { System.out.println("\t"+r+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t UN ");
+	            { System.out.println("\t"+d+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t UN ");
 				}else{
-				  System.out.println("\t"+r+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t" + t);
+				  System.out.println("\t"+d+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t" + t);
 	            }
-	        r = r + i; 
+	        d = d + i; 
 	    }
 	}
 }
